32dbcf8f40351a65415a79bfc1828ac26d46e53a
ssovogg <susuco77@gmail.com>
2022-10-05 11:49:25 +0900

    AuthNavigation 스타일 업데이트

--- src/components/AuthNavigation/AuthNavigation.jsx
+++ src/components/AuthNavigation/AuthNavigation.jsx
@@ -1,33 +1,33 @@
 // 2022.10.04 - 기본 구조 작업
 
 import React from 'react';
+import Button from '../UI/Button/Button';
 import classes from './AuthNavigation.module.css';
 
 const AuthNavigation = () => {
   return (
     <div className={classes.container}>
       <div className={classes.logoArea}>
-        <img className={classes.logo} src='#' alt='logo' />
+        <img className={classes.logo} src='/image/logo.png' alt='logo' />
       </div>
       <div className={classes.authArea}>
         <form className={classes.authForm}>
           <input type='email' placeholder='EMAIL' />
           <input type='password' placeholder='PASSWORD' />
-          <button>로그인</button>
+          <Button className={classes.loginBtn}>로그인</Button>
         </form>
       </div>
       <div className={classes.signUpArea}>
         <span>회원가입</span>
       </div>
       <div className={classes.externalLoginArea}>
-        <button>
-          <img src='#' alt='icon ' />
+        <Button className={classes.externalLogin}>
+          <img src='/image/google.png' alt='google 로그인' />
           구글로 로그인
-        </button>
-        <button>
-          <img src='#' alt='icon ' />
+        </Button>
+        <Button className={classes.externalLogin}>
           게스트 로그인
-        </button>
+        </Button>
       </div>
     </div>
   );
