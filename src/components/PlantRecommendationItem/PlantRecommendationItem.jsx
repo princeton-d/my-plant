f97d1d16dfc4e240f461ece9bd1a3366fef84e85
ssovogg <susuco77@gmail.com>
2022-10-05 18:30:02 +0900

    PlantRecommendationItem 스타일링 중

--- src/components/PlantRecommendationItem/PlantRecommendationItem.jsx
+++ src/components/PlantRecommendationItem/PlantRecommendationItem.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import classes from './PlantRecommendationItem.module.css';
+import React from "react";
+import classes from "./PlantRecommendationItem.module.css";
 
 const PlantRecommendationItem = ({ item }) => {
   return (
-    <>
-      <label>
-        <input type='checkbox' />
-        {item.emoji}
-        {item.text}
+    <div className={classes.recommend}>
+      <label htmlFor={item.id}>
+        <span className={classes.emoji}>{item.emoji}</span>
+        <span className={classes.text}>{item.text}</span>
+        <input
+          type="radio"
+          id={item.id}
+          className={classes.checkbox}
+          name="recommand"
+        />
       </label>
-    </>
+    </div>
   );
 };
 
