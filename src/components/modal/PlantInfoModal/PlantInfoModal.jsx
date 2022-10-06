383a282408f40d3d707d93f433109ec8b468fc2b
ssovogg <susuco77@gmail.com>
2022-10-06 16:29:35 +0900

    PlantInfoModal 스타일링

--- src/components/modal/PlantInfoModal/PlantInfoModal.jsx
+++ src/components/modal/PlantInfoModal/PlantInfoModal.jsx
@@ -1,59 +1,79 @@
-import React from 'react';
-import classes from './PlantInfoModal.module.css';
+import React from "react";
+import classes from "./PlantInfoModal.module.css";
 
 const PlantInfoModal = (props) => {
   console.log(props.plant);
   const onClick = (e) => {
-    if (e.target.id === 'modalClose') {
+    if (e.target.id === "modalClose") {
       props.setOpenModal(!props.openModal);
     }
   };
   return (
     <>
-      <div id='modalClose' className={classes.container} onClick={onClick}>
+      <div id="modalClose" className={classes.container} onClick={onClick}>
         <div className={classes.contents}>
           <div className={classes.imgArea}>
-            <img
-              className={classes.mainImg}
-              src={props.plant.picture[0]}
-              key={Math.random()}
-              alt='img'
-            />
+            <div className={classes.mainImgArea}>
+              <img
+                className={classes.mainImg}
+                src={props.plant.picture[0]}
+                key={Math.random()}
+                alt="img"
+              />
+            </div>
             <div className={classes.subImgArea}>
               {props.plant.picture.map((item) => {
                 return (
-                  <img
-                    className={classes.subImg}
-                    src={item}
-                    key={Math.random()}
-                  />
+                  <div>
+                    <img
+                      className={classes.subImg}
+                      src={item}
+                      key={Math.random()}
+                    />
+                  </div>
                 );
               })}
             </div>
           </div>
           <div className={classes.infoArea}>
-            <p>{props.plant.name}</p>
-            <p>난이도: {props.plant.Difficulty[1]}</p>
-            <p>{props.plant.Description}</p>
+            <div className={classes.topArea}>
+              <h2>{props.plant.name}</h2>
+              <p>
+                <span>난이도</span>
+                <b>{props.plant.Difficulty[1]}</b>
+              </p>
+            </div>
             <div className={classes.detailInfo}>
-              <div>
-                <p>{props.plant.amountOfSunshine[0]}</p>
-                <p>{props.plant.amountOfSunshine[1]}</p>
-              </div>
-              <div>
-                <p>{props.plant.irrigationWater[0]}</p>
-                <p>{props.plant.irrigationWater[1]}</p>
-              </div>
-              <div>
-                <p>잘 자라는 온도</p>
-                <p>
-                  {props.plant.Temperature[0]}~
-                  {props.plant.Temperature[props.plant.Temperature.length - 1]}
-                  ℃의 온도에서 잘 자라요.
-                </p>
+              <p>{props.plant.Description}</p>
+              <div className={classes.detailInfos}>
+                <div>
+                  <img src="/image/sun.png" alt="sun" />
+                  <h3>{props.plant.amountOfSunshine[0]}</h3>
+                  <span>{props.plant.amountOfSunshine[1]}</span>
+                </div>
+                <div>
+                  <img src="/image/water.png" alt="water" />
+                  <h3>{props.plant.irrigationWater[0]}</h3>
+                  <span>{props.plant.irrigationWater[1]}</span>
+                </div>
+                <div>
+                  <img src="/image/air.png" alt="water" />
+                  <h3>잘 자라는 온도</h3>
+                  <span>
+                    {props.plant.Temperature[0]}~
+                    {
+                      props.plant.Temperature[
+                        props.plant.Temperature.length - 1
+                      ]
+                    }
+                    ℃의 온도에서 잘 자라요.
+                  </span>
+                </div>
               </div>
             </div>
-            <button>정원에 담기</button>
+            <div className={classes.buttonArea}>
+              <button>정원에 담기</button>
+            </div>
           </div>
         </div>
       </div>
