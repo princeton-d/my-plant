6d5676f34c6fb77b938e0c74599be12e2991e134
ssovogg <susuco77@gmail.com>
2022-10-07 11:16:10 +0900

    GardenItem nickname edit mode 추가

--- src/components/GardenItem/GardenItem.jsx
+++ src/components/GardenItem/GardenItem.jsx
@@ -1,8 +1,21 @@
 // 김동현 2022.10.04
 import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
 import classes from "./GardenItem.module.css";
 
 const GardenItem = () => {
+  const [editMode, setEditMode] = useState(false);
+  const [nickName, setNickName] = useState("식이");
+  const toggleEditMode = () => {
+    setEditMode((prev) => !prev)
+  };
+  const onChangeNickName = (e) => {
+    setNickName(e.target.value);
+  };
+  const onEditNickName = (e) => {
+    e.preventDefault();
+  }
   return (
     <>
       <li className={classes.item}>
@@ -10,10 +23,27 @@ const GardenItem = () => {
           <img className={classes.plantImg} src="#" alt="plant" />
         </div>
         <div className={classes.rightSide}>
-          <div className={classes.nickNameArea}>
-            <h3 className={classes.plantNickName}>식이</h3>
-            <button><i className="fa-solid fa-pen"></i></button>
-          </div>
+          {editMode ? (
+            <form className={classes.editNickNameForm} onSubmit={onEditNickName}>
+              <input type="text" value={nickName} onChange={onChangeNickName} />
+              <button
+                type="submit"
+                className={classes.editButton}
+              >
+                <i className="fa-solid fa-check"></i>
+              </button>
+              <button onClick={toggleEditMode} className={classes.cancleButton}>
+                x
+              </button>
+            </form>
+          ) : (
+            <div className={classes.nickNameArea}>
+              <h3 className={classes.plantNickName}>{nickName}</h3>
+              <button onClick={toggleEditMode}>
+                <i className="fa-solid fa-pen"></i>
+              </button>
+            </div>
+          )}
           <button className={classes.plantName}>
             <span>블루스타펀</span>
             <span>?</span>
