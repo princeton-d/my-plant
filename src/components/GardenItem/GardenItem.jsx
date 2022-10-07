0f5157f9f333ebabd7536cac66120e58e3f97537
ssovogg <susuco77@gmail.com>
2022-10-07 10:00:57 +0900

    GardenItem 수정 버튼 추가

--- src/components/GardenItem/GardenItem.jsx
+++ src/components/GardenItem/GardenItem.jsx
@@ -1,24 +1,27 @@
 // 김동현 2022.10.04
-import React from 'react';
-import classes from './GardenItem.module.css';
+import React from "react";
+import classes from "./GardenItem.module.css";
 
 const GardenItem = () => {
   return (
     <>
       <li className={classes.item}>
         <div className={classes.plantImgWrap}>
-          <img className={classes.plantImg} src='#' alt='plant' />
+          <img className={classes.plantImg} src="#" alt="plant" />
         </div>
         <div className={classes.rightSide}>
-          <h3 className={classes.plantNickName}>식이</h3>
+          <div className={classes.nickNameArea}>
+            <h3 className={classes.plantNickName}>식이</h3>
+            <button><i className="fa-solid fa-pen"></i></button>
+          </div>
           <button className={classes.plantName}>
             <span>블루스타펀</span>
             <span>?</span>
           </button>
           <p>가장 최근 물 준 날</p>
-          <input type='date' defaultValue='2022-10-05' />
+          <input type="date" defaultValue="2022-10-05" />
           <p>다음 물 줄 날</p>
-          <input type='date' />
+          <input type="date" />
         </div>
       </li>
     </>
