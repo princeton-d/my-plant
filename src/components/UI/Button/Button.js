af32a5266055b052006964fcf1537ef449deb7a0
ssovogg <susuco77@gmail.com>
2022-10-12 16:18:18 +0900

    스타일링 수정 및 경로 이동 수정

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,6 @@ import classes from './Button.module.css';
 
 const Button = (props) => {
   const styles = `${classes.button} ${props.className}`;
-
   return (
     <button className={styles} onClick={props.onClick}>{props.children}</button>
   )
