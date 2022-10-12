c218699c6295b4a923fadfc5626d0ebe93e148dc
websteak <ssk0080@gmail.com>
2022-10-12 15:55:06 +0900

    component remake

--- src/components/router.js
+++ src/components/router.js
@@ -5,12 +5,14 @@ import UserGarden from '../routes/UserGarden/UserGarden';
 import PlantRecommendation from '../routes/PlantRecommendation/PlantRecommendation'
 import Navigation from '../components/Navigation/Navigation';
 import AuthNavigation from './AuthNavigation/AuthNavigation';
+import RecommendationNavigation from './RecommendationNavigation/RecommendationNavigation';
 
 const AppRouter = (props) => {
   return (
     <Router>
       {/* 김동현 2022.10.07 로그인 후 네비게이션 바 보임 */}
       {props.isLogin ? <Navigation userInfo={props.userInfo} setIsLogin={props.setIsLogin} setPlantRecommendation={props.setPlantRecommendation} /> : <AuthNavigation setIsLogin={props.setIsLogin} setUserInfo={props.setUserInfo} />}
+      {props.plantRecommendation && <RecommendationNavigation setPlantRecommendation={props.setPlantRecommendation} />}
       <Routes>
         {props.isLogin ? <Route path='/my-plant' element={<PlantPick userInfo={props.userInfo} />} /> : <Route path='/my-plant' element={<Home />} />}
         <Route path='/my-plant/garden' element={<UserGarden userInfo={props.userInfo} />} />
--- src/routes/PlantRecommendation/PlantRecommendation.jsx
+++ src/routes/PlantRecommendation/PlantRecommendation.jsx
@@ -12,69 +12,10 @@ import PlantItem from '../../components/PlantItem/PlantItem';
 
 // 김동현 2022.10.02
 const PlantRecommendation = (props) => {
-  const navigate = useNavigate();
-
-  const onClick = () => {
-    props.setPlantRecommendation(false);
-    navigate('/my-plant');
-  };
   return (
     <>
       <div className={classes.wrapper}>
         <div className={classes.container}>
-          <div className={classes.navigation}>
-            <div className={classes.container}>
-              {/* logo */}
-              <div className={classes.logoArea}>
-                <img className={classes.logo} src={logo} alt='logo' />
-              </div>
-              <div className={classes.topInfo}>
-                <h3>나는</h3>
-                {checkbox.topCheckbox.map((item) => {
-                  return (
-                    <PlantRecommendRadioBig
-                      item={item}
-                      key={item.text}
-                      name='aboutMe'
-                    />
-                  );
-                })}
-              </div>
-              <div className={classes.middleInfo}>
-                <h3>우리집은</h3>
-                <div className={classes.middleBtn}>
-                  {checkbox.middleCheckbox.map((item) => {
-                    return (
-                      <PlantRecommendRadioSmall
-                        item={item}
-                        key={item.text}
-                        name='aboutHome1'
-                      />
-                    );
-                  })}
-                  {checkbox.middleCheckboxTwo.map((item) => {
-                    return (
-                      <PlantRecommendRadioSmall
-                        item={item}
-                        key={item.text}
-                        name='aboutHome2'
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-              <div className={classes.bottomInfo}>
-                <h3>이런걸 원해요</h3>
-                {checkbox.bottomCheckbox.map((item) => {
-                  return <PlantRecommendCheckbox item={item} key={item.text} />;
-                })}
-              </div>
-              <button className={classes.backBtn} onClick={onClick}>
-                <i className='fa-solid fa-angle-left'></i>
-              </button>
-              <span className={classes.recommendBtn}>추천 식물 보기 ↓</span>
-            </div>
-          </div>
           <div className={classes.title}>
             <p>
               {props.userInfo && props.userInfo.displayName}님을 위한{' '}
