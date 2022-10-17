de7766ba9fc2133297888277f4c0ba4b5abfc96c
websteak <ssk0080@gmail.com>
2022-10-17 13:02:01 +0900

    오늘의 식물 pick 랜덤으로 나오게

--- src/routes/PlantPick/PlantPick.jsx
+++ src/routes/PlantPick/PlantPick.jsx
@@ -4,20 +4,27 @@ import classes from './PlantPick.module.css';
 import plantInfo from '../../data/plantInfo.json';
 
 // 김동현 2022.10.04 - 기본작업
-const PlantPick = ({userInfo}) => {
+const PlantPick = ({ userInfo }) => {
   let userId = userInfo.uid;
+  const randomPlant = (array) => {
+    array.sort(() => Math.random() - 0.5);
+
+    return array;
+  };
 
   return (
     <>
       <div className={classes.wrapper}>
         <div className={classes.container}>
           <div className={classes.title}>
-            <p>오늘의 식물 <span>PICK</span></p>
+            <p>
+              오늘의 식물 <span>PICK</span>
+            </p>
           </div>
           <div className={classes.toadyPlant}>
             <ul>
               {/* 김동현 2022.10.04 - 각각의 식물 정보 mapping */}
-              {plantInfo.plant.map((item) => {
+              {randomPlant(plantInfo.plant).map((item) => {
                 return <PlantItem plant={item} key={item.id} userId={userId} />;
               })}
             </ul>
