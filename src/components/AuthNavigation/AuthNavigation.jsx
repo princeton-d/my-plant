834be9fc71a5b979b333363d10f1dd1712c262bc
websteak <ssk0080@gmail.com>
2022-10-22 21:52:55 +0900

    default value(email, password)설정

--- src/components/AuthNavigation/AuthNavigation.jsx
+++ src/components/AuthNavigation/AuthNavigation.jsx
@@ -5,21 +5,21 @@ import {
   signInAnonymously,
   signInWithEmailAndPassword,
   signInWithPopup,
-} from "firebase/auth";
-import React, { useState } from "react";
-import { authService } from "../../service/fbase";
-import Button from "../UI/Button/Button";
-import classes from "./AuthNavigation.module.css";
-import ModalPortal from "../../components/modal/modalPortal";
-import SignUpModal from "../../components/modal/SignUpModal/SignUpModal";
-import logo from "../../image/logo.png";
-import googleLogo from "../../image/google.png";
+} from 'firebase/auth';
+import React, { useState } from 'react';
+import { authService } from '../../service/fbase';
+import Button from '../UI/Button/Button';
+import classes from './AuthNavigation.module.css';
+import ModalPortal from '../../components/modal/modalPortal';
+import SignUpModal from '../../components/modal/SignUpModal/SignUpModal';
+import logo from '../../image/logo.png';
+import googleLogo from '../../image/google.png';
 
 const AuthNavigation = (props) => {
   const [openModal, setOpenModal] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [email, setEmail] = useState('test@test.com');
+  const [password, setPassword] = useState('123123');
+  const [error, setError] = useState('');
   // 김동현 2022.10.06 - 모달창 실행
   const handleSignUpButton = () => {
     setOpenModal(!openModal);
@@ -27,9 +27,9 @@ const AuthNavigation = (props) => {
 
   // 김동현 2022.10.05 - 이메일, 비밀번호 state에 저장
   const onChange = (e) => {
-    if (e.target.name === "email") {
+    if (e.target.name === 'email') {
       setEmail(e.target.value);
-    } else if (e.target.name === "password") {
+    } else if (e.target.name === 'password') {
       setPassword(e.target.value);
     }
   };
@@ -44,11 +44,11 @@ const AuthNavigation = (props) => {
       );
     } catch (error) {
       switch (error.code) {
-        case "auth/wrong-password":
-          setError("잘못된 비밀번호입니다.");
+        case 'auth/wrong-password':
+          setError('잘못된 비밀번호입니다.');
           break;
-        case "auth/user-not-found":
-          setError("등록되지 않은 계정입니다.");
+        case 'auth/user-not-found':
+          setError('등록되지 않은 계정입니다.');
           break;
         default:
           setError(error.code);
@@ -68,22 +68,24 @@ const AuthNavigation = (props) => {
     <div className={classes.container}>
       <div className={classes.wrap}>
         <div className={classes.logoArea}>
-          <img className={classes.logo} src={logo} alt="logo" />
+          <img className={classes.logo} src={logo} alt='logo' />
         </div>
         <div className={classes.authArea}>
           <form className={classes.authForm} onSubmit={onSubmit}>
             <input
-              name="email"
-              type="email"
+              name='email'
+              type='email'
               required
-              placeholder="EMAIL"
+              placeholder='EMAIL'
+              defaultValue='test@test.com'
               onChange={onChange}
             />
             <input
-              name="password"
-              type="password"
+              name='password'
+              type='password'
               required
-              placeholder="PASSWORD"
+              placeholder='PASSWORD'
+              defaultValue='123123'
               onChange={onChange}
             />
             <Button className={classes.loginBtn}>로그인</Button>
@@ -103,7 +105,7 @@ const AuthNavigation = (props) => {
             className={classes.externalLogin}
             onClick={handleGoogleLoginButton}
           >
-            <img src={googleLogo} alt="google 로그인" />
+            <img src={googleLogo} alt='google 로그인' />
             구글로 로그인
           </Button>
           <Button className={classes.externalLogin} onClick={handleGuestButton}>
